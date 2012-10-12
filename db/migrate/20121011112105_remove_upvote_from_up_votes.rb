e4dd422f32aa6902a56d04bf7aaed2b1a9e3346d
Nithya Rajaram <nithya@muziboo.com>
2012-10-12 17:16:50 -0300

    Removed unnecessary migrations

--- db/migrate/20121011112105_remove_upvote_from_up_votes.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUpvoteFromUpVotes < ActiveRecord::Migration
-  def up
-    remove_column :up_votes, :upvote
-  end
-
-  def down
-    add_column :up_votes, :upvote, :integer
-  end
-end
--- db/migrate/20121011112359_add_columns_to_up_votes.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToUpVotes < ActiveRecord::Migration
-  def change
-    add_column :up_votes, :user_id, :integer
-    add_column :up_votes, :user_ip, :string
-    add_column :up_votes, :upvotable_id, :integer
-    add_column :up_votes, :upvotable_type, :string
-  end
-end
