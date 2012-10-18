6e7858fb8e7757b9f720116698d27b77d0f1e61b
Nithya Rajaram <nithya@muziboo.com>
2012-10-18 18:16:48 -0300

    Ajaxifying votes for comments and replies

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -3,7 +3,7 @@ class UpvotesController < ApplicationController
    before_filter :authenticate_user!, :only => [:create]
 
   def create
-    @upvote = Upvote.create(params[:upvotable])
+    @upvote = Upvote.new(params[:upvote])
     @upvote.upvotable_type = params[:upvotable_type]
     @upvote.upvotable_id = params[:upvotable_id]
     @user = current_user
