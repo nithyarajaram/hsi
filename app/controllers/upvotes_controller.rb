1bcf3d16f15b803089526d380bdef74e97248024
Nithya Rajaram <nithya@muziboo.com>
2012-11-02 15:32:41 -0300

    jquery and ajax for upvoting posts

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -3,19 +3,24 @@ class UpvotesController < ApplicationController
    before_filter :authenticate_user!, :only => [:create]
 
   def create
-    @upvote = Upvote.new(params[:upvote])
+    @upvote = Upvote.new(params[:upvotes])
     @upvote.upvotable_type = params[:upvotable_type]
     @upvote.upvotable_id = params[:upvotable_id]
+    @url = params[:url]
+    @post = Post.find(@upvote.upvotable_id)
     @user = current_user
     @upvote.user_id = @user.id
     @upvote.user_ip = @user.last_sign_in_ip
     @upvote.save
 
     respond_to do |format|
-      format.html { redirect_to params[:url]}
-      format.js
+      if @upvote.save
+        format.json { render :json => @post.upvotes.count }
+      else
+        format.html { render @url }
+      end
     end
+  end
 
-  end 
+end 
 
-end
